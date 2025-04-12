9e0d59e2c67ece4bb6066d7c9c50568e2b68d638
Observersss <heavyobjectics@gmail.com>
2025-04-13 01:35:27 +0300

    add echo for show user a progress

--- lab1/scripts/hello.sh
+++ lab1/scripts/hello.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 set -e
 
+echo "Starting build..."
+
 # Go to the project folder
 cd /project/dragon-tiger
 
 # Create a test Tiger file with a simple print statement
 echo 'print("Hello World!\n")' > hello.tig
 
+echo "Running compiler..."
+
 # Compile the Tiger file to an object file
 src/driver/dtiger -o hello.o hello.tig
 
+echo "Linking..."
+
 # Link the object file with the runtime library to create an executable
 clang++ hello.o src/runtime/posix/libruntime.a -o hello -no-pie
+
+echo "Running compiled program:"
 
 # Run the compiled program
 ./hello
