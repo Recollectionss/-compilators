dd628c967e5d1ec5f462312cf75b7a224fc640ea
Observersss <heavyobjectics@gmail.com>
2025-04-13 02:08:26 +0300

    add template code for hello task

--- lab1/scripts/hello.sh
+++ lab1/scripts/hello.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 set -e
 
-echo "Starting build..."
+source scripts/source/messages.sh
+
+print_start
 
 # Go to the project folder
 cd /project/dragon-tiger
 
 # Create a test Tiger file with a simple print statement
-echo 'print("Hello World!\n")' > hello.tig
+cp ../scripts/source/hello.tig.template hello.tig
 
-echo "Running compiler..."
+print_compile
 
 # Compile the Tiger file to an object file
 src/driver/dtiger -o hello.o hello.tig
 
-echo "Linking..."
+print_link
 
 # Link the object file with the runtime library to create an executable
 clang++ hello.o src/runtime/posix/libruntime.a -o hello -no-pie
 
-echo "Running compiled program:"
+print_run
 
 # Run the compiled program
 ./hello
