202cbfc32c860af03452de9238246af7d3bb19df
Observersss <heavyobjectics@gmail.com>
2025-04-13 02:08:04 +0300

    add template code for fibonacci, and use messages.sh

--- lab1/scripts/fibonacci.sh
+++ lab1/scripts/fibonacci.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 set -e
 
-echo "Starting build..."
+source scripts/source/messages.sh
+
+print_start
 
 # Go to the project folder
 cd /project/dragon-tiger
 
-cat > fibonacci.tig <<EOF
-let
-  function fibonacci(n : int) : int =
-    if n < 3 then 1
-    else fibonacci(n - 1) + fibonacci(n - 2)
-in
-  for i := 1 to 8 do
-    (print_int(fibonacci(i)); print("\n"))
-end
-EOF
+cp ../scripts/source/fibonacci.tig.template fibonacci.tig
 
-
-echo "Running compiler..."
+print_compile
 
 # Compile the Tiger file to an object file
 src/driver/dtiger -o fibonacci.o fibonacci.tig
 
-echo "Linking..."
+print_link
 
 clang++ fibonacci.o src/runtime/posix/libruntime.a -o fibonacci -no-pie
 
-echo "Running compiled program:"
+print_run 
 
 # Run the compiled program
 ./fibonacci
