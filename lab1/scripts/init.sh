74d082458e74ce97a5946905f3380a1b7570dc5c
Observersss <heavyobjectics@gmail.com>
2025-04-26 19:25:36 +0300

    Some changes

--- lab1/scripts/init.sh
+++ lab1/scripts/init.sh
@@ -4,3 +4,5 @@ set -e
 docker buildx build --platform=linux/amd64 -t tiger-lab-1 .
 
 docker run -it --rm tiger-lab-1
+
+docker rmi tiger-lab-1
--- lab1/scripts/read_unsigned/run.sh
+++ lab1/scripts/read_unsigned/run.sh
@@ -3,14 +3,21 @@ set -e
 
 echo
 echo "Input: 123"
-echo -n "Result: "
+echo "Result: "
 
 echo "123" | ./dragon-tiger/read_unsigned
 echo
 
 
 echo "Input: 123f"
-echo -n "Result: "
+echo "Result: "
+
+echo "123f" | ./dragon-tiger/read_unsigned
+echo
+
+
+echo "Input: -123"
+echo "Result: "
 
 echo "123f" | ./dragon-tiger/read_unsigned
 echo
